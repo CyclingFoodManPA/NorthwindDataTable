d29a5a2e16fa5cecf3ac021a997a7e403ce643dd
Keith S. Safford <ks.safford@verizon.net>
2021-05-31 19:12:06 -0400

    Add project files.

---
+++ NorthwindDataTable/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NorthwindDataTable.Context;
+using NorthwindDataTable.ViewModels;
+
+namespace NorthwindDataTable.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AjaxMethod()
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            var vm = (from c in db.Customers
+                      select new CustomerListViewModel
+                      {
+                        CustomerID = c.CustomerID,
+                        CompanyName = c.CompanyName,
+                        ContactName = c.ContactName,
+                        City = c.City,
+                        Country = c.Country
+                      }).ToList();
+            return Json(vm);
+        }
+    }
+}
